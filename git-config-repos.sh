cf316517743acfa692a874922a20f23c59ae637d
Luis Palacios <luis.palacios.derqui@gmail.com>
2024-09-22 17:10:43 +0200

    Soporte para WSL2

--- git-config-repos.sh
+++ git-config-repos.sh
@@ -34,6 +34,9 @@
 #
 # ----------------------------------------------------------------------------------------
 
+# Trap ctrl-c and call ctrl_c()
+trap ctrl_c INT
+
 # ----------------------------------------------------------------------------------------
 # Variables Globales
 # ----------------------------------------------------------------------------------------
@@ -100,21 +103,98 @@ echo_status() {
 # Utility Functions
 # ----------------------------------------------------------------------------------------
 
+# Función de controlador de señal para manejar CTRL-C
+function ctrl_c() {
+  echo "** Abortado por CTRL-C"
+  exit
+}
+
+# Función para convertir una ruta de WSL a una ruta de Windows
+# Si no se puede convertir se sale del programa porque se considera
+# un error en la configuración del archivo JSON y es grave
+#
+# Ejemplo de uso
+# windos_global_folder=$(convert_wsl_to_windows_path $global_folder)
+#
+#
+convert_wsl_to_windows_path() {
+    local wsl_path="$1"
+
+    # Comprobar si el path empieza con /mnt/
+    if [[ "$wsl_path" =~ ^/mnt/([a-zA-Z])/ ]]; then
+        # Extraer la letra de la unidad
+        local drive_letter=$(echo "${BASH_REMATCH[1]}" | tr '[:lower:]' '[:upper:]')
+
+        # Remover el prefijo /mnt/<unidad>/
+        local path_without_prefix="${wsl_path#/mnt/${BASH_REMATCH[1]}/}"
+
+        # Reemplazar las barras inclinadas (/) con barras invertidas (\)
+        local windows_path=$(echo "$path_without_prefix" | sed 's|/|\\|g')
+
+        # Formar la ruta final en el formato de Windows
+        echo "${drive_letter}:\\${windows_path}"
+    else
+        echo "Error: La ruta $global_folder no está en el formato esperado de WSL2."
+        echo "Revisa global.folder en el .json, asegúrate de que comience con /mnt/<unidad>/"
+        exit 1
+    fi
+}
+
+# Definición de la función wcm_search para buscar credenciales en Windows Credential Manager
+wcm_search() {
+    local target="$1"
+    local user="$2"
+
+    # Ejecutar cmdkey y guardar la salida
+    output=$(cmd.exe /c "cmdkey /list" | tr -d '\r')
+
+    # Buscar el bloque que contiene el target y el usuario
+    match=$(echo "$output" | awk -v tgt="$target" -v usr="$user" '
+        $0 ~ "Target:" && $0 ~ tgt {found_tgt=1}
+        found_tgt && $0 ~ "User:" && $0 ~ usr {found_usr=1}
+        found_tgt && found_usr {print_block=1}
+        print_block && $0 ~ /^$/ {exit}
+        print_block {print}
+    ')
+
+    # Comprobar si se encontró el bloque
+    if [ -n "$match" ]; then
+        #echo "$match"
+        return 0
+    else
+        #echo "No se encontró ninguna coincidencia para Target: $target y User: $user"
+        return 1
+    fi
+}
+
+# Función para comprobar la existencia de un comando
+check_command() {
+    if ! which "$1" > /dev/null; then
+        echo "Error: $1 no se encuentra en el PATH."
+        return 1
+    else
+        return 0
+    fi
+}
+
 # Function to check if a credential is stored in the credential manager
 check_credential_in_store() {
 
     case "$OSTYPE" in
       # MacOS
       darwin*|freebsd*)
+        # OSX Keychain
         security find-generic-password -s "git:$1" -a "$2" &>/dev/null
         return $?
         ;;
       # Linux
       *)
         if [ ${IS_WSL2} == true ]; then
-            echo "ERROR TODAVIA NO SE SOPORTA WINDOWS WSL2"
-            return 1
+            # Windows Credential Manager
+            wcm_search "git:$1" "$2"
+            return $?
         else
+            # Linux Secret Service
             output=$(secret-tool search service "git:$1" account "$2" 2>/dev/null)
             line_count=$(echo "$output" | wc -l)
             if [ "$line_count" -gt 1 ]; then
@@ -130,7 +210,7 @@ check_credential_in_store() {
 # Dependencias del Script
 # ----------------------------------------------------------------------------------------
 
-# Array de dependencias
+# PROGRAMAS que deben estar intalados
 programs=("git" "jq")
 
 # Compruebo las dependencias
@@ -155,32 +235,78 @@ for program in "${programs[@]}"; do
     fi
 done
 
+# En WSL2 comprobar si cmd.exe y git.exe están en el PATH
+# Nota, en WSL2 uso git.exe en vez de git de ubuntu porque es compatible con
+# el Credential Manager de Windows
+if [ ${IS_WSL2} == true ]; then
+    # Comprobar cmd.exe
+    check_command "cmd.exe"
+    cmd_status=$?
+
+    # Comprobar git.exe
+    check_command "git.exe"
+    git_status=$?
+
+    # Sugerencias para añadir al PATH si no se encuentran
+    if [ $cmd_status -ne 0 ] || [ $git_status -ne 0 ]; then
+        echo ""
+        echo "Añade al PATH:"
+
+        if [ $cmd_status -ne 0 ]; then
+            echo "  - /mnt/c/Windows/System32 (para cmd.exe)"
+        fi
+
+        if [ $git_status -ne 0 ]; then
+            echo "  - /mnt/c/Program Files/Git/mingw64/bin (para git.exe)"
+            echo "Te recomiendo que instales Git for Windows desde https://git-scm.com/download/win"
+        fi
+
+        echo "Para hacerlo permanente, añádelo a ~/.zshrc, ~/.bashrc o ~/.profile."
+        exit 1
+    fi
+fi
+
 # ----------------------------------------------------------------------------------------
 # Main Script Execution
 # ----------------------------------------------------------------------------------------
 
-# Cargar y parsear el archivo JSON usando jq
-config_file="$HOME/.config/git-config-repos/git-config-repos.json"
-echo_message "* Config $config_file"
-if [ ! -f "$config_file" ]; then
+# Ficheros de configuración
+if [ ${IS_WSL2} == true ]; then
+    # En WSL2 uso git.exe
+    git_command="git.exe"
+    # En WSL2 trabajo sobre el disco de Windows
+    #git_global_config_file="/mnt/c/Users/${USER}/.gitconfig"
+    # Cargar y parsear el archivo JSON usando jq
+    git_config_repos_json_file="/mnt/c/Users/${USER}/.config/git-config-repos/git-config-repos.json"
+else
+    # En Mac y Linux el comando git es git
+    git_command="git"
+    # En Mac y Linux el home del usuario
+    #git_global_config_file="${HOME}/.gitconfig"
+    # Cargar y parsear el archivo JSON usando jq
+    git_config_repos_json_file="$HOME/.config/git-config-repos/git-config-repos.json"
+fi
+echo_message "* Config $git_config_repos_json_file"
+if [ ! -f "$git_config_repos_json_file" ]; then
     echo_status error
-    echo "ERROR: El archivo de configuración $config_file no existe."
+    echo "ERROR: El archivo de configuración $git_config_repos_json_file no existe."
     exit 1
 fi
 
 # Validar el archivo JSON con jq
-jq '.' "$config_file" > /dev/null 2>&1
+jq '.' "$git_config_repos_json_file" > /dev/null 2>&1
 if [ $? -ne 0 ]; then
     echo_status error
-    echo "ERROR: El archivo JSON $config_file contiene errores de sintaxis."
+    echo "ERROR: El archivo JSON $git_config_repos_json_file contiene errores de sintaxis."
     exit 1
 fi
 echo_status ok
 
+
 # Extraer configuraciones globales
-global_folder=$(jq -r '.global.folder' "$config_file")
-credential_helper=$(jq -r '.global.credential.helper' "$config_file")
-credential_store=$(jq -r '.global.credential.credentialStore' "$config_file")
+global_folder=$(jq -r '.global.folder' "$git_config_repos_json_file")
+credential_helper=$(jq -r '.global.credential.helper' "$git_config_repos_json_file")
+credential_store=$(jq -r '.global.credential.credentialStore' "$git_config_repos_json_file")
 
 # Crear el directorio global de Git
 echo_message "Directorio Git: $global_folder"
@@ -194,32 +320,29 @@ echo_status ok
 
 # Configurar globalmente Git
 echo_message "Configuración de Git global"
-git config --global credential.helper "$credential_helper"
-git config --global credential.credentialStore "$credential_store"
-accounts=$(jq -r '.accounts | keys[]' "$config_file")
+$git_command config --global credential.helper "$credential_helper"
+$git_command config --global credential.credentialStore "$credential_store"
+accounts=$(jq -r '.accounts | keys[]' "$git_config_repos_json_file")
 for account in $accounts; do
-    account_credential_url=$(jq -r ".accounts[\"$account\"].credential_url" "$config_file")
-    account_username=$(jq -r ".accounts[\"$account\"].username" "$config_file")
-    account_provider=$(jq -r ".accounts[\"$account\"].provider" "$config_file")
-    account_useHttpPath=$(jq -r ".accounts[\"$account\"].useHttpPath" "$config_file")
+    account_credential_url=$(jq -r ".accounts[\"$account\"].credential_url" "$git_config_repos_json_file")
+    account_username=$(jq -r ".accounts[\"$account\"].username" "$git_config_repos_json_file")
+    account_provider=$(jq -r ".accounts[\"$account\"].provider" "$git_config_repos_json_file")
+    account_useHttpPath=$(jq -r ".accounts[\"$account\"].useHttpPath" "$git_config_repos_json_file")
     # Configurar las credenciales globales para la cuenta
-    git config --global credential."$account_credential_url".provider "$account_provider"
-    git config --global credential."$account_credential_url".useHttpPath "$account_useHttpPath"
-    #git config --global credential."$account_credential_url".username "$account_username"
+    $git_command config --global credential."$account_credential_url".provider "$account_provider"
+    $git_command config --global credential."$account_credential_url".useHttpPath "$account_useHttpPath"
 done
 echo_status ok
 
-
 # CREDENCIALES
 # Iterar sobre las cuentas para los CREDENCIALES
-accounts=$(jq -r '.accounts | keys[]' "$config_file")
+accounts=$(jq -r '.accounts | keys[]' "$git_config_repos_json_file")
 for account in $accounts; do
-    account_url=$(jq -r ".accounts[\"$account\"].url" "$config_file")
-    account_credential_url=$(jq -r ".accounts[\"$account\"].credential_url" "$config_file")
-    account_username=$(jq -r ".accounts[\"$account\"].username" "$config_file")
+    account_url=$(jq -r ".accounts[\"$account\"].url" "$git_config_repos_json_file")
+    account_credential_url=$(jq -r ".accounts[\"$account\"].credential_url" "$git_config_repos_json_file")
+    account_username=$(jq -r ".accounts[\"$account\"].username" "$git_config_repos_json_file")
 
     # Avisar al usuario para que prepare el navegador para que se autentique
-
     echo_message "Comprobando credenciales de $account > $account_username"
     check_credential_in_store "$account_credential_url" "$account_username"
     if [ $? -eq 0 ]; then
@@ -228,14 +351,14 @@ for account in $accounts; do
         echo_status warning
         read -p "Preapara tu navegador para autenticar a $account > $account_username - (Enter/Ctrl-C)." confirm
         credenciales="/tmp/tmp-credenciales"
-        ( echo url="$account_credential_url"; echo "username=$account_username"; echo ) | git credential fill > $credenciales 2>/dev/null
+        ( echo url="$account_credential_url"; echo "username=$account_username"; echo ) | $git_command credential fill > $credenciales 2>/dev/null
         if [ -f $credenciales ] && [ ! -s $credenciales ]; then
             # No deberia entrar por aquí
             echo "    Ya se habián configurado las credenciales en el pasado"
             echo "$account/$username ya tiene los credenciales configurados en el almacen"
         else
             echo_message "    Añado las credenciales al almacen de credenciales"
-            cat $credenciales | git credential approve
+            cat $credenciales | $git_command credential approve
             echo_status ok
         fi
     fi
@@ -243,19 +366,19 @@ done
 
 # REPORITORIOS
 # Iterar sobre las cuentas para los REPOSITORIOS
-accounts=$(jq -r '.accounts | keys[]' "$config_file")
+accounts=$(jq -r '.accounts | keys[]' "$git_config_repos_json_file")
 for account in $accounts; do
-    account_url=$(jq -r ".accounts[\"$account\"].url" "$config_file")
-    account_clone_url=$(jq -r ".accounts[\"$account\"].clone_url" "$config_file")
-    account_credential_url=$(jq -r ".accounts[\"$account\"].credential_url" "$config_file")
-    account_username=$(jq -r ".accounts[\"$account\"].username" "$config_file")
-    account_folder=$(jq -r ".accounts[\"$account\"].folder" "$config_file")
-    account_subfolder=$(jq -r ".accounts[\"$account\"].subfolder" "$config_file")
+    account_url=$(jq -r ".accounts[\"$account\"].url" "$git_config_repos_json_file")
+    account_clone_url=$(jq -r ".accounts[\"$account\"].clone_url" "$git_config_repos_json_file")
+    account_credential_url=$(jq -r ".accounts[\"$account\"].credential_url" "$git_config_repos_json_file")
+    account_username=$(jq -r ".accounts[\"$account\"].username" "$git_config_repos_json_file")
+    account_folder=$(jq -r ".accounts[\"$account\"].folder" "$git_config_repos_json_file")
+    account_subfolder=$(jq -r ".accounts[\"$account\"].subfolder" "$git_config_repos_json_file")
     if [ $account_subfolder == "null" ]; then
         account_subfolder=""
     fi
-    account_provider=$(jq -r ".accounts[\"$account\"].provider" "$config_file")
-    account_useHttpPath=$(jq -r ".accounts[\"$account\"].useHttpPath" "$config_file")
+    account_provider=$(jq -r ".accounts[\"$account\"].provider" "$git_config_repos_json_file")
+    account_useHttpPath=$(jq -r ".accounts[\"$account\"].useHttpPath" "$git_config_repos_json_file")
 
     # Crear el directorio para la cuenta
     echo_message "  $account_folder"
@@ -268,10 +391,10 @@ for account in $accounts; do
     echo_status ok
 
     # Iterar sobre los repositorios de la cuenta
-    repos=$(jq -r ".accounts[\"$account\"].repos | keys[]" "$config_file")
+    repos=$(jq -r ".accounts[\"$account\"].repos | keys[]" "$git_config_repos_json_file")
     for repo in $repos; do
-        repo_name=$(jq -r ".accounts[\"$account\"].repos[\"$repo\"].name" "$config_file")
-        repo_email=$(jq -r ".accounts[\"$account\"].repos[\"$repo\"].email" "$config_file")
+        repo_name=$(jq -r ".accounts[\"$account\"].repos[\"$repo\"].name" "$git_config_repos_json_file")
+        repo_email=$(jq -r ".accounts[\"$account\"].repos[\"$repo\"].email" "$git_config_repos_json_file")
 
         # Crea estructura de carpeta(s) si el repo tiene un subdir
         if [ -n "$account_subfolder" ]; then
@@ -284,7 +407,15 @@ for account in $accounts; do
         if [ ! -d "$repo_path" ]; then
             echo         "   - $repo_path"
             echo_message "    ⬇ $repo_path"
-            git clone "$account_clone_url/$repo.git" "$repo_path" &>/dev/null
+
+            # Si estamos en WSL2 convertir la ruta de destino del clone a formato C:\..
+            if [ ${IS_WSL2} == true ]; then
+                destination_directory=$(convert_wsl_to_windows_path $repo_path) # En WSL2
+            else
+                destination_directory=$repo_path # En Mac y Linux
+            fi
+            # Clonar el repo
+            $git_command clone "$account_clone_url/$repo.git" "$destination_directory" &>/dev/null
             if [ $? -eq 0 ]; then
                 echo_status ok
             else
@@ -298,11 +429,11 @@ for account in $accounts; do
 
         # Configurar el repositorio local
         cd "$repo_path" || continue
-        git remote set-url origin "$account_url/$repo.git"
-        git config user.name "$repo_name"
-        git config user.email "$repo_email"
-        git remote set-url --push origin "$account_url/$repo.git"
-        git config credential."$account_credential_url".username "$account_username"
+        $git_command remote set-url origin "$account_url/$repo.git"
+        $git_command config user.name "$repo_name"
+        $git_command config user.email "$repo_email"
+        $git_command remote set-url --push origin "$account_url/$repo.git"
+        $git_command config credential."$account_credential_url".username "$account_username"
 
     done
 done
